85b590f536d085f18365f194dac84b3ddf4d4688
Shannon Joyner <saj9191@gmail.com>
2014-01-31 19:21:17 -0500

    Changed model direction to velocity and added position

--- js/Model.js
+++ js/Model.js
@@ -1,8 +1,10 @@
 // Information associated with a model in the animation
-function Model(name, direction, action, imageLink) {
+function Model(name, position, velocity, action, imageLink) {
   this.name = name;
-  // Direction the model is moving
-  this.direction = direction;
+  // Current position of model
+  this.position = position
+  // Current velocity of model
+  this.velocity = velocity;
   // What the object is doing (for now let's make the actions
   // stationary or moving
   this.action = action;
@@ -14,12 +16,20 @@ Model.prototype.getName = function() {
   return this.name;
 }
 
-Model.prototype.getDirection = function() {
-  return this.direction;
+Model.prototype.getPosition = function() {
+  return this.position;
 }
 
-Model.prototype.setDirection = function(direction) {
-  this.direction = direction;
+Model.prototype.setPosition = function(position) {
+  this.position = position
+}
+
+Model.prototype.getVelocity = function() {
+  return this.velocity;
+}
+
+Model.prototype.setVelocity = function(velocity) {
+  this.velocity = velocity;
 }
 
 Model.prototype.getAction = function() {
